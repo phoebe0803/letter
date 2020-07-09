437cc18556f22890ad02a5f539ac10d9b57609e6
BlueGrey <bluegrey@BlueGreydeMacBook-Pro.local>
2020-07-09 21:25:49 +0800

    add session to setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+APPEND_SLASH=False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp'
 ]
+SESSION_ENGINE='django.contrib.sessions.backends.cache'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
